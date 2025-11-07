2755b2b3f87b4a48199590f8f45300898ba67721
egijlstra <egijlstr@students.kennesaw.edu>
2025-11-07 15:49:03 -0500

    final updates
    pushing final updates to match

--- 6 Web Page with Flask/basic page/script1.py
+++ 6 Web Page with Flask/basic page/script1.py
@@ -26,5 +26,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/Erik/')
+def Erik():
+    return render_template("gijlstra.html")
+
 if __name__=="__main__":
     app.run(debug=True)
